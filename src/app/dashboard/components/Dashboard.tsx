12e17c5e7f3a2837c3069e01f6e90b1938bfd460
felicio.angga <211110176@students.mikroskil.ac.id>
2024-12-19 11:29:46 +0700

    revision

--- src/app/dashboard/components/Dashboard.tsx
+++ src/app/dashboard/components/Dashboard.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import DiscussionList from "./DiscussionList";
-import CreateDiscussion from "./CreateDiscussion";
 
 export default function () {
   return (
     <div className="px-5 lg:px-10 py-5 flex flex-col lg:flex-row gap-5 lg:justify-center xl:justify-center">
+      Admin
       {/* <CreateDiscussion /> */}
       <DiscussionList />
     </div>
